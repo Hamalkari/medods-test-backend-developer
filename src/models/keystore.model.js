558a24229c30fba869523063c346cd37d6255c22
Halmakari <kosha.1997@bk.ru>
2020-12-08 20:26:22 +0300

    Add keystore model

---
+++ src/models/keystore.model.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const KeystoreSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    blacklisted: {
+      type: Schema.Types.Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+KeystoreSchema.index({ user: 1, refreshToken: 1 });
+
+KeystoreSchema.pre('save', async function () {
+  const keystore = this;
+  const SALT_ROUNDS = 10;
+
+  if (!keystore.isModified('refreshToken')) return;
+
+  const refreshTokenHash = await bcrypt.hash(
+    keystore.refreshToken,
+    SALT_ROUNDS,
+  );
+
+  keystore.refreshToken = refreshTokenHash;
+
+  await keystore.save();
+});
+
+const KeystoreModel = model('KeyStore', KeystoreSchema, 'keystores');
+
+module.exports = KeystoreModel;
